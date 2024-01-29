85b42a951f450acfc4a708ba47df930b822bc5bd
Gamal Ahmed <102909147+gamalahmedd@users.noreply.github.com>
2024-01-29 05:29:23 +0200

    Graduation Start

---
+++ GraduationEmbeddedSide/GraduationEmbeddedSide/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.c
+
+MCAL\GIE\GIE.c
+
+MCAL\UART\UART.c
+
+MCAL\UART\UART_cfg.c
+
+MCAL\UART\UART_service.c
+
