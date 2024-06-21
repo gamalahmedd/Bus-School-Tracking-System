fa5ec6ac9fe0da64efdd1f19163ef56869e2f207
Gamal Ahmed <102909147+gamalahmedd@users.noreply.github.com>
2024-06-21 20:51:25 +0300

    Add System Initialization

--- GraduationControllerSide/GraduationControllerSide/Service/service.c
+++ GraduationControllerSide/GraduationControllerSide/Service/service.c
@@ -1,6 +1,25 @@
 #include "service.h"
 
+
 portSHORT startScheduler()
 {
+	System_Init();
 	
+	
+}
+
+void System_Init()
+{
+	ESP01_Init();
+	Accelerometor_init();
+	LCD_Init();
+	mfrc522_init();
+}
+
+void ESP01_CheckConnectionTask(void *pvParam)
+{
+	while(1)
+	{
+		
+	}
 }
--- GraduationControllerSide/GraduationControllerSide/Service/service.h
+++ GraduationControllerSide/GraduationControllerSide/Service/service.h
@@ -3,9 +3,17 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
+#include "event_groups.h"
+#include "semphr.h"
+#include "../../HAL/ADXL345/ADXL345.h"
+#include "../../HAL/LCD/LCD.h"
+#include "../../HAL/ESP01/ESP01.h"
+#include "../../HAL/MFRC/mfrc522.h"
 #include <avr/interrupt.h>
 
-
+void System_Init(void);
+void ESP01_CheckConnectionTask(void *pvParam);
 portSHORT startScheduler();
 
 #endif /* SERVICE_H_ */
